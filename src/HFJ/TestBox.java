124c5854d690ac85aaccc1b4e68b6417eb5d4679
Manujitha Perera <manujithaperera@gmail.com>
2022-06-14 11:20:43 +0530

    Waterfall first question done!

---
+++ src/HFJ/TestBox.java
@@ -0,0 +1,15 @@
+package HFJ;
+
+public class TestBox {
+    Integer i;
+    int j;
+    public static void main (String[] args) {
+        TestBox t = new TestBox();
+        t.go();
+    }
+    public void go() {
+        j=i;
+        System.out.println(j);
+        System.out.println(i);
+    }
+}
